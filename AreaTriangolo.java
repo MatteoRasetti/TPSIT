2d280af83dde3f59d3453761b81f1215b1652889
Matteo Rasetti <matteo.rasetti@libero.it>
2017-01-12 16:02:56 +0100

    Update AreaTriangolo.java

--- AreaTriangolo.java
+++ AreaTriangolo.java
@@ -1,23 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Matteo Rasetti                                  4°IN B                                         12/01/2016
+Prendere in input i dati di un triangolo, e successivamente calcolare perimetro ed area, stampando i risultati.
  */
 package area.triangolo;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Sheax
- */
 public class AreaTriangolo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner input = new Scanner(System.in);
 
         float l1, l2, h, b;
